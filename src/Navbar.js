69fdefdf01fae1bf6b29282be7782539468f1a60
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-11-20 20:12:15 +1100

    Added responsiveness to the navbar, installed line icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,37 +1,67 @@
-import React, { Fragment } from "react";
-import { NavLink } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { NavLink, useMatch } from "react-router-dom";
 import "./Navbar.css";
 import logo from "./images/logo.png";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <Fragment>
       <div className="container-fluid Navbar">
-        <nav className="navbar navbar-expand-lg fixed-top d-flex justify-content-between px-4 my-navbar">
+        <nav className="navbar navbar-expand-lg fixed-top px-4 my-navbar">
           <NavLink to="/" className="navbar-brand">
             <img src={logo} alt="logo" />
           </NavLink>
-          <ul className="navbar-nav">
-            <li className="nav-item px-3">
-              <NavLink to="/" className="nav-link" activeClassName="active">
-                HOME
-              </NavLink>
-            </li>
-            <li className="nav-item px-3">
-              <NavLink to="/menu" className="nav-link" activeClassName="active">
-                MENU
-              </NavLink>
-            </li>
-            <li className="nav-item px-3 ">
-              <NavLink
-                to="/reservations"
-                className="nav-link"
-                activeClassName="active"
-              >
-                RESERVATIONS
-              </NavLink>
-            </li>
-          </ul>
+          <button
+            onClick={toggle}
+            className={`navbar-toggler ${isOpen ? "" : "collapsed"}`}
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            className={`${
+              isOpen ? "collapse show" : "collapse"
+            } navbar-collapse`}
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item px-3">
+                <NavLink
+                  to="/"
+                  className={`nav-link ${useMatch("/") ? "active" : ""}`}
+                >
+                  HOME
+                </NavLink>
+              </li>
+              <li className="nav-item px-3">
+                <NavLink
+                  to="/menu"
+                  className={`nav-link ${useMatch("/menu") ? "active" : ""}`}
+                >
+                  MENU
+                </NavLink>
+              </li>
+              <li className="nav-item px-3 ">
+                <NavLink
+                  to="/reservations"
+                  className={`nav-link ${
+                    useMatch("/reservations") ? "active" : ""
+                  }`}
+                >
+                  RESERVATIONS
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </Fragment>
