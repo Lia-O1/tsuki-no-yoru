0051dd43501ffe8e438603ac7c5cb584846cc6e5
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-11-29 22:42:39 +1100

    Footer styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,25 @@
 import React from "react";
+import "../styles/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="Footer">
-      <div className="d-flex justify-content-evenly">
-        <div>Location 2/5 Imaginary St Melbourne VIC</div>
-        <div>Open Hours Tue-Sun, 5PM-11PM Closed mondays</div>
-        <div>Contact Us (07) 4246 7575 tsukinoyoru@gmail.com</div>
+    <div className="Footer py-5">
+      <div className="d-flex justify-content-evenly text-uppercase py-5">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <span>Location</span>
+          <span>2/5 Imaginary St</span>
+          <span>Melbourne VIC</span>
+        </div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <span>Open Hours</span>
+          <span>Tue-Sun, 5PM-11PM</span>
+          <span>Closed mondays</span>
+        </div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <span>Contact Us</span>
+          <span>(07) 4246 7575</span>
+          <span>tsukinoyoru@gmail.com</span>
+        </div>
       </div>
     </div>
   );
