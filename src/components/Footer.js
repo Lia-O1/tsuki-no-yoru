d0c323dcc8649f6fa5b37249ca7b44ce28b3387a
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-12-03 19:25:05 +1100

    Refactored Footer.js to use Bootstrap's grid system. Made it responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,23 @@ import "../styles/Footer.css";
 export default function Footer() {
   return (
     <div className="Footer py-5">
-      <div className="d-flex justify-content-evenly text-uppercase py-5">
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <span>Location</span>
-          <span>2/5 Imaginary St</span>
-          <span>Melbourne VIC</span>
-        </div>
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <span>Open Hours</span>
-          <span>Tue-Sun, 5PM-11PM</span>
-          <span>Closed mondays</span>
-        </div>
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <span>Contact Us</span>
-          <span>(07) 4246 7575</span>
-          <span>tsukinoyoru@gmail.com</span>
+      <div className="container">
+        <div className="row d-flex justify-content-evenly text-uppercase">
+          <div className="col-md-4 d-flex flex-column justify-content-center align-items-center py-4">
+            <span>Location</span>
+            <span>2/5 Imaginary St</span>
+            <span>Melbourne VIC</span>
+          </div>
+          <div className="col-md-4 d-flex flex-column justify-content-center align-items-center py-4">
+            <span>Open Hours</span>
+            <span>Tue-Sun, 5PM-11PM</span>
+            <span>Closed mondays</span>
+          </div>
+          <div className="col-md-4 d-flex flex-column justify-content-center align-items-center py-4">
+            <span>Contact Us</span>
+            <span>(07) 4246 7575</span>
+            <span>tsukinoyoru@gmail.com</span>
+          </div>
         </div>
       </div>
     </div>
