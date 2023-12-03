cc542e450a8ea649f2356ba78715947b9c3d96f7
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-12-03 19:14:52 +1100

    Made Welcome.js responsive

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,13 +6,15 @@ export default function Welcome() {
   return (
     <div className="d-flex justify-content-center align-items-center Welcome">
       <div className="d-flex flex-column justify-content-center align-items-cente text-center bg">
-        <h1 className="mb-0">TSUKI NO YORU</h1>
-
-        <p className="mb-5">
-          | SAVOR THE AUTHENTIC FLAVOURS OF JAPAN WITH EVERY BITE |
+        <h1 className="mt-5 mb-0">TSUKI NO YORU</h1>
+        <hr className="b-block d-md-none" />
+        <p className="mb-4 mx-auto">
+          <span className="d-none d-md-inline">|</span> SAVOR THE AUTHENTIC
+          FLAVOURS OF JAPAN WITH EVERY BITE{" "}
+          <span className="d-none d-md-inline">|</span>
         </p>
 
-        <button className="mt-2 form-button">
+        <button className="mb-5 form-button">
           {" "}
           <NavLink to="/reservations" className="link">
             BOOK A TABLE
